c7b55770ac224644c1e1482f748484839f6042df
Andrew MacGinitie <macginitie@charter.net>
2020-09-12 22:20:28 -0400

    a few more baby steps in procedural style

--- src/main/scala/SqrtMain.scala
+++ src/main/scala/SqrtMain.scala
@@ -1,13 +1,31 @@
 object SqrtMain {
 
   def main(args: Array[String]): Unit = {
-    for (s <- args) {
-      println(s)
+    val s = args(0)
+    if (check_arg(s)) {
+      val num = dbl(s)
+      val root = Newton(4.0)
+      println(s"the square root of "+args(0)+s" is approximately ${root}")
     }
-    println(s"the square root of "+args(0)+" is approximately 2")
+    else {
+      // 2DO: complain politely
+      println("sorry, dunno how to find the square root of "+args(0))
+      println("please restrict input to digits, with at most one decimal point")
+    }
+  }
+
+  def check_arg(s: String): Boolean = {
+    // 2DO
+    true
+  }
+
+  def dbl(s: String): Double = {
+    // 2DO
+    4.0
   }
 
   def Newton(num: Double): Double = {
+    // 2DO
     2.0
   }
 }
