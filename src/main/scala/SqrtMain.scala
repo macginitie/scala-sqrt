68e250021ebf7a2eb0e06d888fd4dd0a87592955
Andrew MacGinitie <macginitie@charter.net>
2020-09-13 16:06:02 -0400

    functional in the sense of 'it works' but not yet functional in the programming language sense

--- src/main/scala/SqrtMain.scala
+++ src/main/scala/SqrtMain.scala
@@ -1,7 +1,7 @@
-object SqrtMain {
+import scala.math
+import scala.math.pow
 
-  val max_level = 5
-  var answers = Array(1.0,0,0,0,0)
+object SqrtMain {
 
   def main(args: Array[String]): Unit = {
     val s = args(0)
@@ -10,14 +10,17 @@ object SqrtMain {
       println("the square root of 0 is 0")
     }
     else if (num > 0) {
-      val root = Newton(num, answers, max_level, 1)
+      val max_level = 15
+      var estimates = Array(1.0, 0.0)
+
+      val root = Newton(num, estimates, max_level, 1)
       println(s"the square root of ${num} is approximately ${root}")
     }
     else {
       // complain politely
-      println("sorry, I don't how to find the square root of "+args(0))
+      println("sorry, I don't how to find the square root of " + s)
       println("please restrict input to digits, with at most one decimal point")
-      println("(this program doesn't handle complex numbers, but valid scientific notation is accepted)")
+      println("(this program doesn't handle complex numbers, \nbut valid scientific notation is accepted)")
     }
   }
 
@@ -27,14 +30,17 @@ object SqrtMain {
     s.toDoubleOption.getOrElse(-1d)
   }
 
-  def Newton(in_num: Double, answers: Array[Double], max_level:Int, level:Int): Double = {
-    if (level == max_level) {
-      answers(max_level-1)
-    }
+  def Newton(in_num: Double, estimates: Array[Double], max_level:Int, level:Int): Double = {
+    println(s"${estimates(0)}, ${estimates(1)}")
+    if (level == max_level)
+      estimates(1)
     else {
       // 2DO
-      // calculate answer(level) using answer(level-1)
-      Newton(in_num, answers, max_level, level + 1)
+      // calculate new estimate using previous estimate
+      var prev_est = estimates(0)
+      estimates(1) = prev_est - ((pow(prev_est,2) - in_num)/(2 * prev_est))
+      estimates(0) = estimates(1)
+      Newton(in_num, estimates, max_level, level + 1)
     }
   }
 }
