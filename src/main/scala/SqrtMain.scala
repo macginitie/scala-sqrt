8d5768330a31b5231d9867ff8c73cbce4200e229
Andrew MacGinitie <macginitie@charter.net>
2020-09-14 20:18:48 -0400

    added a little bit of functional programming flavor

--- src/main/scala/SqrtMain.scala
+++ src/main/scala/SqrtMain.scala
@@ -1,8 +1,9 @@
-import scala.math
 import scala.math.pow
 
 object SqrtMain {
 
+  // program entry point which accepts
+  // arguments provided on the command line
   def main(args: Array[String]): Unit = {
     val s = args(0)
     val num = dbl(s)
@@ -10,37 +11,56 @@ object SqrtMain {
       println("the square root of 0 is 0")
     }
     else if (num > 0) {
-      val max_level = 15
-      var estimates = Array(1.0, 0.0)
+      val max_level = 12
+      var estimate = Array(1.0)
 
-      val root = Newton(num, estimates, max_level, 1)
+      val root = square_root(num, estimate, max_level, 1, Newton)
       println(s"the square root of ${num} is approximately ${root}")
     }
     else {
-      // complain politely
+      // the program doesn't handle valid negative numbers, or anything that fails
+      // to parse to a double, which is flagged as -1 in the dbl() function
       println("sorry, I don't how to find the square root of " + s)
       println("please restrict input to digits, with at most one decimal point")
-      println("(this program doesn't handle complex numbers, \nbut valid scientific notation is accepted)")
+      println("(this program doesn't handle negative numbers,")
+      println("but valid scientific notation is accepted)")
     }
   }
 
   // parse s to extract Double;
-  // return -1.0 to indicate failure
+  // returns -1.0 to indicate failure to parse,
+  // so that negative numbers and invalid arguments
+  // can be handled with the same code
   def dbl(s: String): Double = {
     s.toDoubleOption.getOrElse(-1d)
   }
 
-  def Newton(in_num: Double, estimates: Array[Double], max_level:Int, level:Int): Double = {
-    println(s"${estimates(0)}, ${estimates(1)}")
+  // recursive function to approximate square root using an iterative method
+  // in_num: the number for which the square root is sought
+  // estimate: a 1-element array that stores the current estimate
+  // max_level: number of iterations of the method (i.e., number of recursive calls)
+  // level: current level of recursion
+  // estimate_reviser: function that produces estimate x[n] from x[n-1] and the input number
+  def square_root(in_num: Double,
+                  estimates: Array[Double],
+                  max_level:Int,
+                  level:Int,
+                  estimate_reviser: (Double, Double) => Double
+                 ): Double = {
+    // useful for debugging:
+    // println(s"${level} ==> ${estimate(0)}, ${estimate(1)}")
     if (level == max_level)
-      estimates(1)
+      estimates(0)
     else {
-      // 2DO
       // calculate new estimate using previous estimate
       var prev_est = estimates(0)
-      estimates(1) = prev_est - ((pow(prev_est,2) - in_num)/(2 * prev_est))
-      estimates(0) = estimates(1)
-      Newton(in_num, estimates, max_level, level + 1)
+      estimates(0) = estimate_reviser(in_num, prev_est)
+      square_root(in_num, estimates, max_level, level + 1, estimate_reviser)
     }
+  }
+
+  // single iteration of Newton's Method
+  def Newton(in_num: Double, prev_est: Double): Double = {
+    prev_est - ((pow(prev_est,2) - in_num)/(2 * prev_est))
   }
 }
